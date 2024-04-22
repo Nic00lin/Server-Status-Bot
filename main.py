69e37cdbdf3698a251ed00f29f41c7643a56847b
Nic00lin <pavanik646@gmail.com>
2024-04-22 22:43:38 +0300

    Added end

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ users_credentials = {}
 
 welcome_message = """
 Привет! Я бот для подключения по SSH к удаленной системе.
-Чтобы начать, отправьте команду /start.
 """
 
 @bot.message_handler(commands=['start'])
@@ -58,8 +57,63 @@ def ask_password(message):
 
 def generate_commands_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row('/reboot', '/free')
+    markup.row('/info', '/free', '/ports', '/services', '/reboot')
+    markup.row('Перезапустить бота')
     return markup
+
+
+@bot.message_handler(func=lambda message: message.text == "Перезапустить бота")
+def handle_start(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    user_markup.row('Перезапуск бота')
+    bot.send_message(message.from_user.id, welcome_message, reply_markup=user_markup)
+    user_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    user_markup.row('Подключиться к удаленному хосту')
+@bot.message_handler(commands=['info'])
+def reboot_command(message):
+    chat_id = message.chat.id
+    if chat_id not in users_credentials:
+        bot.reply_to(message, "Сначала выполните команду /ssh для подключения к удаленной системе.")
+        return
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(users_credentials[chat_id]['ip'], port=users_credentials[chat_id]['port'],
+                    username=users_credentials[chat_id]['username'], password=users_credentials[chat_id]['password'])
+
+        stdin, stdout, stderr = ssh.exec_command("cat /etc/*release")
+        output = stdout.read().decode()
+        bot.reply_to(message, f"Команда получения информации выполнена: {output}")
+        bot.send_message(chat_id, "Выберите действие:", reply_markup=generate_actions_markup())
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при выполнении команды: {str(e)}")
+    finally:
+        ssh.close()
+
+@bot.message_handler(commands=['ports'])
+def reboot_command(message):
+    chat_id = message.chat.id
+    if chat_id not in users_credentials:
+        bot.reply_to(message, "Сначала выполните команду /ssh для подключения к удаленной системе.")
+        return
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(users_credentials[chat_id]['ip'], port=users_credentials[chat_id]['port'],
+                    username=users_credentials[chat_id]['username'], password=users_credentials[chat_id]['password'])
+
+        stdin, stdout, stderr = ssh.exec_command("ss -tunpl")
+        output = stdout.read().decode()
+        bot.reply_to(message, f"Информация об используемых портах: {output}")
+        bot.send_message(chat_id, "Выберите действие:", reply_markup=generate_actions_markup())
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при выполнении команды: {str(e)}")
+    finally:
+        ssh.close()
 
 @bot.message_handler(commands=['reboot'])
 def reboot_command(message):
@@ -78,6 +132,29 @@ def reboot_command(message):
         stdin, stdout, stderr = ssh.exec_command("sudo reboot")
         output = stdout.read().decode()
         bot.reply_to(message, f"Команда перезагрузки выполнена: {output}")
+        bot.send_message(chat_id, "Выберите действие:", reply_markup=generate_actions_markup())
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при выполнении команды перезагрузки: {str(e)}")
+    finally:
+        ssh.close()
+
+@bot.message_handler(commands=['services'])
+def reboot_command(message):
+    chat_id = message.chat.id
+    if chat_id not in users_credentials:
+        bot.reply_to(message, "Сначала выполните команду /ssh для подключения к удаленной системе.")
+        return
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(users_credentials[chat_id]['ip'], port=users_credentials[chat_id]['port'],
+                    username=users_credentials[chat_id]['username'], password=users_credentials[chat_id]['password'])
+
+        stdin, stdout, stderr = ssh.exec_command("service --status-all")
+        output = stdout.read().decode()
+        bot.reply_to(message, f"Информация о запущенных сервисах: {output}")
         bot.send_message(chat_id, "Выберите действие:", reply_markup=generate_actions_markup())
     except Exception as e:
         bot.reply_to(message, f"Ошибка при выполнении команды перезагрузки: {str(e)}")
@@ -109,7 +186,7 @@ def free_command(message):
 
 def generate_actions_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row('Подключиться к другому серверу', 'Выполнить другую команду')
+    markup.row('Подключиться к другому серверу', 'Выбрать другую команду')
     return markup
 
 @bot.message_handler(func=lambda message: message.text == "Подключиться к другому серверу")
@@ -117,8 +194,8 @@ def connect_to_another_server(message):
     del users_credentials[message.chat.id]
     start_ssh_connection(message)
 
-@bot.message_handler(func=lambda message: message.text == "Выполнить другую команду")
-def execute_another_command(message):
-    bot.reply_to(message, "Введите новую команду для выполнения:")
+@bot.message_handler(func=lambda message: message.text == "Выбрать другую команду")
+def choose_another_command(message):
+    bot.send_message(message.chat.id, "Выберите команду для выполнения:", reply_markup=generate_commands_markup())
 
 bot.polling()
